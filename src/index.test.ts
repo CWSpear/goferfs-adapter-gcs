7d2a9c2d646de779629103022094e79482972ace
Cameron Spear <cameronspear@gmail.com>
2017-02-01 14:51:48 -0800

    No env vars Travis?

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,6 +6,11 @@ const API_KEY = process.env.GCS_AKI_KEY;
 const BUCKET = process.env.GCS_BUCKET;
 const PROJECT_ID = process.env.GCS_PROJECT_ID;
 
+console.log(process.env.TEST_ENV_VAR);
+if (!process.env.TEST_ENV_VAR) {
+    console.log(process.env);
+}
+
 describe('Google Cloud Storage Adapter', function () {
     this.timeout(10000);
 
